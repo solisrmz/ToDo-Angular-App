bbd257507dba54ea39926b465372cfeb0dd07e89
Solis <imsolis@hotmail.com>
2020-09-07 11:06:54 -0500

    He agregado mis proyectos

--- src/app/Proyectos/my-projects/my-projects.component.ts
+++ src/app/Proyectos/my-projects/my-projects.component.ts
@@ -1,9 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import {trigger, style, animate, transition, state} from '@angular/animations';
 
 @Component({
   selector: 'app-my-projects',
   templateUrl: './my-projects.component.html',
-  styleUrls: ['./my-projects.component.css']
+  styleUrls: ['./my-projects.component.css'],
+  animations:[
+    trigger('enterState', [
+      state('void', style({
+        transform: 'translateX(-100%)',
+        opacity:0
+      })),
+      transition(':enter', [
+        animate(500, style({
+          transform: 'translateX(0)',
+          opacity:1
+        }))
+      ])
+    ])
+]
 })
 export class MyProjectsComponent implements OnInit {
 
