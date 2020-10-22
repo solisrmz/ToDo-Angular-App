9bd56eed65e25fe0d683d757ea99355049e63362
solis <solis@localhost.localdomain>
2020-10-22 13:56:59 -0500

    Cambios en rutas e imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {ListComponent} from './components/Tareas/list/list.component';
-import {AddComponent} from './components/Tareas/add/add.component';
-import {EditComponent} from './components/Tareas/edit/edit.component';
-import {WelcomeComponent} from './components/welcome/welcome.component';
-import {ContactoComponent} from './components/contacto/contacto.component';
-import {LoginComponent} from './components/Tareas/Login/login/login.component';
-import {MyProjectsComponent} from './components/Proyectos/my-projects/my-projects.component';
+import {ListComponent} from './Components/Tareas/list/list.component';
+import {AddComponent} from './Components/Tareas/add/add.component';
+import {EditComponent} from './Components/Tareas/edit/edit.component';
+import {WelcomeComponent} from './Components/welcome/welcome.component';
+import {ContactoComponent} from './Components/contacto/contacto.component';
+import {LoginComponent} from './Components/Tareas/Login/login/login.component';
+import {MyProjectsComponent} from './Components/Proyectos/my-projects/my-projects.component';
 import {UserGuardService} from './Guard/user-guard.service';
 
 const routes: Routes = [
-  {path:'', component:WelcomeComponent},
-  {path:'login', component:LoginComponent},
-  {path:'list', component:ListComponent, canActivate: [UserGuardService]},
-  {path:'create', component:AddComponent, canActivate: [UserGuardService]},
-  {path: 'projects', component:MyProjectsComponent},
-  {path: 'contacto', component:ContactoComponent},
-  {path: 'tarea/:id', component:EditComponent, canActivate: [UserGuardService]}
+  {path: '', component: WelcomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'list', component: ListComponent, canActivate: [UserGuardService]},
+  {path: 'create', component: AddComponent, canActivate: [UserGuardService]},
+  {path: 'projects', component: MyProjectsComponent},
+  {path: 'contacto', component: ContactoComponent},
+  {path: 'tarea/:id', component: EditComponent, canActivate: [UserGuardService]}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ListComponent } from './components/Tareas/list/list.component';
-import { AddComponent } from './components/Tareas/add/add.component';
-import { EditComponent } from './components/Tareas/edit/edit.component';
+import { ListComponent } from './Components/Tareas/list/list.component';
+import { AddComponent } from './Components/Tareas/add/add.component';
+import { EditComponent } from './Components/Tareas/edit/edit.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {TareaServiceService} from './Service/tarea-service.service';
 import {AuthInterceptorService} from './Service/auth-interceptor.service';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import {UserGuardService} from './Guard/user-guard.service';
 import {LoginService} from './Service/login.service';
-import { WelcomeComponent } from './components/welcome/welcome.component';
+import { WelcomeComponent } from './Components/welcome/welcome.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { MyProjectsComponent } from './components/Proyectos/my-projects/my-projects.component';
-import { LoginComponent } from './components/Tareas/Login/login/login.component';
+import { MyProjectsComponent } from './Components/Proyectos/my-projects/my-projects.component';
+import { LoginComponent } from './Components/Tareas/Login/login/login.component';
 import { NgxSpinnerModule } from "ngx-spinner";  
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { ContactoComponent } from './components/contacto/contacto.component';
+import { ContactoComponent } from './Components/contacto/contacto.component';
 
 
 @NgModule({
